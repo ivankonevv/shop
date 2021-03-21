ab07da54ce959d16d2a738a91f52fa368b571506
Ivan <ivankonewvv@yandex.by>
2021-03-21 16:56:45 +0300

    View added. Details url added. Image cropping disabled.

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,11 +3,20 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from PIL import Image
+from django.urls import reverse
 # from io import BytesIO
 # import sys
 # from django.core.files.uploadedfile import InMemoryUploadedFile
 
 User = get_user_model()
+
+
+def get_product_url(obj, viewname):
+    ct_model = obj.__class__._meta.model_name
+    return reverse(viewname, kwargs={
+        'ct_model': ct_model,
+        'slug': obj.slug,
+    })
 
 
 class MinResolutionErrorException(Exception):
@@ -128,6 +137,9 @@ class Notebook(Product):
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
 
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
+
 
 class Smartphone(Product):
     diagonal = models.CharField(max_length=255, verbose_name="Diagonal")
@@ -146,6 +158,9 @@ class Smartphone(Product):
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
+
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
 
 
 class CartProduct(models.Model):
